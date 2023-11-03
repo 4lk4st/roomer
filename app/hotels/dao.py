65d1e3f453786db758ac64cec9ef1a489be937eb
Зайковский Всеволод <4lk4st@gmail.com>
2023-11-03 13:09:57 +0700

    working...

--- app/hotels/dao.py
+++ app/hotels/dao.py
@@ -25,44 +25,52 @@ class HotelsDAO(BaseDAO):
             FROM bookings
             LEFT JOIN rooms ON bookings.room_id = rooms.id
             LEFT JOIN hotels ON rooms.hotel_id = hotels.id
-            WHERE date_from <= '2023-06-20' AND date_to >= '2023-05-15' AND hotels.location LIKE '%Алтай%'
+            WHERE date_from <= '2023-06-20'
+            AND date_to >= '2023-05-15' AND hotels.location LIKE '%Алтай%'
             GROUP BY rooms.hotel_id
         )
         SELECT hotels.id, hotels.name, hotels.location, hotels.services,
-            hotels.rooms_quantity, hotels.image_id, hotels.rooms_quantity - COALESCE(booked_rooms.count, 0) AS rooms_left
+            hotels.rooms_quantity, hotels.image_id,
+            hotels.rooms_quantity - COALESCE(booked_rooms.count, 0)
+                AS rooms_left
         FROM hotels
         LEFT JOIN booked_rooms ON hotels.id = booked_rooms.hotel_id
-        WHERE hotels.location LIKE '%Алтай%' AND hotels.rooms_quantity - COALESCE(booked_rooms.bookings_count, 0) > 0
+        WHERE hotels.location LIKE '%Алтай%'
+        AND hotels.rooms_quantity
+            - COALESCE(booked_rooms.bookings_count, 0) > 0
         """
         async with async_session_maker() as session:
-            booked_rooms = (select(Rooms.hotel_id,
-                                   func.count(Bookings.id).label("bookings_count"))
-                                   .select_from(Bookings)
-                                   .join(Rooms, Bookings.room_id == Rooms.id, isouter=True)
-                                   .join(Hotels, Rooms.hotel_id == Hotels.id, isouter=True)
-                                   .where(
-                                       and_(Bookings.date_from <= date_to,
-                                            Bookings.date_to >= date_from,
-                                            Hotels.location.like(f'%{location}%')))
-                                   .group_by(Rooms.hotel_id)
-                                   .cte("booked_rooms"))
+            booked_rooms = (
+                select(Rooms.hotel_id,
+                       func.count(Bookings.id).label("bookings_count"))
+                .select_from(Bookings)
+                .join(Rooms, Bookings.room_id == Rooms.id, isouter=True)
+                .join(Hotels, Rooms.hotel_id == Hotels.id, isouter=True)
+                .where(
+                    and_(Bookings.date_from <= date_to,
+                         Bookings.date_to >= date_from,
+                         Hotels.location.like(f'%{location}%')))
+                .group_by(Rooms.hotel_id)
+                .cte("booked_rooms"))
 
-            query = (select(Hotels.id,
-                            Hotels.name,
-                            Hotels.location,
-                            Hotels.services,
-                            Hotels.rooms_quantity,
-                            Hotels.image_id,
-                            (Hotels.rooms_quantity - func.coalesce(
-                                booked_rooms.c.bookings_count, 0)).label("rooms_left"))
-                            .select_from(Hotels)
-                            .join(booked_rooms, Hotels.id == booked_rooms.c.hotel_id, isouter=True)
-                            .where(
-                                and_(Hotels.location.like(f'%{location}%'),
-                                     (Hotels.rooms_quantity - func.coalesce(
-                                      booked_rooms.c.bookings_count, 0)) > 0)))
-
-            print(query.compile(engine, compile_kwargs={"literal_binds": True}))
-            
+            query = (
+                select(Hotels.id,
+                       Hotels.name,
+                       Hotels.location,
+                       Hotels.services,
+                       Hotels.rooms_quantity,
+                       Hotels.image_id,
+                       (Hotels.rooms_quantity - func.coalesce(
+                           booked_rooms.c.bookings_count, 0))
+                           .label("rooms_left"))
+                .select_from(Hotels)
+                .join(booked_rooms,
+                      Hotels.id == booked_rooms.c.hotel_id,
+                      isouter=True)
+                .where(
+                    and_(Hotels.location.like(f'%{location}%'),
+                         (Hotels.rooms_quantity - func.coalesce(
+                             booked_rooms.c.bookings_count, 0)) > 0)))
+           
             result = await session.execute(query)
             return result.mappings().all()
--- app/hotels/rooms/dao.py
+++ app/hotels/rooms/dao.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import date, timedelta
 
 from sqlalchemy import and_, func, select
 from app.bookings.models import Bookings
@@ -7,6 +7,7 @@ from app.dao.base import BaseDAO
 from app.database import async_session_maker, engine
 from app.hotels.models import Hotels
 from app.hotels.rooms.models import Rooms
+
 
 class RoomsDAO(BaseDAO):
     model = Rooms
@@ -29,27 +30,38 @@ class RoomsDAO(BaseDAO):
         )
         SELECT rooms.*,
             ($total_days * price ) AS total_cost,
-            (rooms.quantity - COALESCE(rooms_booked.booking_count, 0)) AS rooms_left
+            (rooms.quantity - COALESCE(rooms_booked.booking_count, 0))
+                AS rooms_left
         FROM rooms
-        LEFT JOIN rooms_booked ON rooms.id = rooms_booked.room_id
+        LEFT JOIN rooms_booked ON rooms.id = rooms_booked.room_id       
         """
+
+        total_days = (date_to - date_from).days
+
         async with async_session_maker() as session:
 
-            rooms_booked = (select(Bookings.room_id,
-                                  func.count(Bookings.id).label("booking_count"))
-                                  .select_from(Bookings)
-                                  .join(Rooms, Bookings.room_id == Rooms.id, isouter=True)
-                                  .where(
-                                      and_(Hotels.id == hotel_id,
-                                           Bookings.date_from <= date_to,
-                                           Bookings.date_to >= date_from))
-                                  .group_by(Bookings.room_id))
+            rooms_booked = (
+                select(Bookings.room_id,
+                       func.count(Bookings.id).label("booking_count"))
+                .select_from(Bookings)
+                .join(Rooms, Bookings.room_id == Rooms.id, isouter=True)
+                .where(
+                    and_(Rooms.hotel_id == hotel_id,
+                         Bookings.date_from <= date_to,
+                         Bookings.date_to >= date_from))
+                .group_by(Bookings.room_id)
+                .cte("rooms_booked"))
 
-            print(rooms_booked.compile(engine, compile_kwargs={"literal_binds": True}))
-            query = 1
-
-
-
+            query = (
+                select(Rooms,
+                       (total_days * Rooms.price).label("total_cost"),
+                       (Rooms.quantity
+                        - func.coalesce(rooms_booked.c.booking_count, 0))
+                       .label("rooms_left"))
+                       .select_from(Rooms)
+                       .join(rooms_booked,
+                             Rooms.id == rooms_booked.c.room_id,
+                             isouter=True))
 
             result = await session.execute(query)
             return result.mappings().all()
